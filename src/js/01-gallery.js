04307c297415cc315c86b14850bf90a976f47b58
PokhilenkoD <pokhilenko.d@gmail.com>
2023-10-31 13:57:42 +0100

    first task completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const list = document.querySelector('.js-gallery');
+list.addEventListener('click', onClick);
+murkup(galleryItems, list);
+
+function murkup(items, element) {
+  const murkup = items
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    )
+    .join('');
+  element.insertAdjacentHTML('beforeend', murkup);
+}
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function onClick(e) {
+  if (!e.target.closest('.gallery__link')) {
+    return;
+  }
+  e.preventDefault();
+}
